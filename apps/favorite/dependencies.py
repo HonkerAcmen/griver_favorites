ca981e3d23716e08d87680dffa3b1d3126ee14dc
Mark Yang <haker0x04@163.com>
2026-07-28 21:40:20 +0800

    fix: Fix the bug of injecting dependencies into get_dab session

--- apps/favorite/dependencies.py
+++ apps/favorite/dependencies.py
@@ -5,5 +5,5 @@ from apps.core.database import get_db_session
 from apps.favorite.services.folder import FolderService
 
 
-def get_folder_service(session:Session = Depends(get_db_session())):
+def get_folder_service(session:Session = Depends(get_db_session)):
         return FolderService(session)
